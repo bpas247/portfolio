51efcec0574e850e4eba6d83ef5fdf29d47fa85b
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-12-01 23:07:58 -0800

    chore: add frowny face

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
   <>
-    <h1>404: NOT FOUND</h1>
+    <h1>404: NOT FOUND :(</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
 );
