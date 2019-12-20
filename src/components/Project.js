470486d4fd5c376075854ccf08ff6fb198798fa0
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-12-20 00:11:42 -0800

    feat: more styling updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,33 @@
 import React from "react";
 import styled from "@emotion/styled";
 
+const StyledProject = styled.div``;
+
+const StyledName = styled.h3`
+  width: 95%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+`;
+
+const StyledPosition = styled.span``;
+
 const Project = ({ name, maintainer, children }) => (
-  <>
-    <h3>
-      {name} | {maintainer ? "Maintainer" : "Contributor"}
-    </h3>
+  <StyledProject>
+    <StyledName>
+      {name}{" "}
+      <StyledPosition>
+        {maintainer ? "Maintainer" : "Contributor"}
+      </StyledPosition>
+    </StyledName>
     {children}
-  </>
+  </StyledProject>
 );
 
 Project.Summary = ({ children }) => <p>{children}</p>;
 
 Project.Contributions = styled.div`
-  padding-bottom: 5%;
+  margin-bottom: 5%;
 `;
 
 export default Project;
